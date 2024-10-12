4962f79a02a7671a62454833e99dea602cd09f5a
zfish85 <68242105+zfish85@users.noreply.github.com>
2024-10-12 12:19:14 +0200

    Finalized MPV

--- src/main.js
+++ src/main.js
@@ -1,3 +1,51 @@
+const rates = ["officialEuro", "creditCard", "street", "transfer", "officialDollar"];
+const euroRates = rates.filter(rate => rate !== "officialDollar");
+
+
+// Init
+
+document.addEventListener('init', function (event) {
+
+
+    if (event.target.classList.contains("page--wrapper")) {
+        document.querySelector(".legal-button").onclick = () => loadPage('legal.html');
+        document.querySelector('ons-tabbar').addEventListener('prechange', setRates);
+
+    }
+
+    if (event.target.id == "rate") {
+        document.querySelector(".save-button").addEventListener("click", () => {
+            saveRates();
+            calculatePesosExchange();
+            calculateDollarExchange();
+        })
+
+
+
+    }
+    if (event.target.id === "pesos") {
+        document.querySelector("#inputPesos").addEventListener("input", () => {
+            calculatePesosExchange();
+        });
+    }
+
+    if (event.target.id === "dollar") {
+        document.querySelector("#inputDollar").addEventListener("input", (event) => {
+            calculateDollarExchange();
+
+        });
+    }
+
+    document.querySelectorAll(".integer-only").forEach((element) => {
+        element.addEventListener('input', function () {
+            restrictToInteger(this);
+        });
+    });
+
+});
+
+// General functions
+
 function loadPage(page) {
     document.querySelector('#navigator').bringPageTop(page, {animation: 'fade'});
 }
@@ -10,7 +58,7 @@ function restrictToInteger(inputElement) {
     }
 }
 
-const  rates = ["officialEuro", "creditCard", "street", "transfer", "officialDollar"];
+// Save Rates
 
 function saveRates() {
     rates.forEach(saveRate);
@@ -18,8 +66,8 @@ function saveRates() {
 
 function saveRate(rate) {
     let value = document.getElementById(rate).value;
-    if(value && value > 0) {
-        localStorage.setItem(rate,value);
+    if (value && value > 0) {
+        localStorage.setItem(rate, value);
     } else {
         localStorage.removeItem(rate);
         document.getElementById(rate).value = "";
@@ -27,41 +75,139 @@ function saveRate(rate) {
     }
 }
 
-function setRates(){
+function setRates() {
     rates.forEach(setRate)
 }
 
-function setRate(rate){
+function setRate(rate) {
     let value = localStorage.getItem(rate);
-    if(value && value > 0){
+    if (value && value > 0) {
         document.getElementById(rate).value = value;
     }
 
 }
 
+//Pesos
+
+function calculatePesosExchange(){
+    let inputPesos = document.querySelector("#inputPesos").value;
+    calculateExchanges(inputPesos, "pesos")
 
 
+}
 
 
-document.addEventListener('init', function (event) {
-    document.querySelector(".legal-button").onclick = () => loadPage('legal.html');
-    document.querySelector('ons-tabbar').addEventListener('prechange', setRates)
-    document.querySelectorAll(".integer-only").forEach((element) => {
-        element.addEventListener('input', function () {
-            restrictToInteger(this);
-        });
-    });
+//Dollar
 
-    if(event.target.id == "rate"){
-        document.querySelector(".save-button").onclick = saveRates;
+function calculateDollarExchange() {
+    let inputDollar = document.querySelector("#inputDollar").value;
+
+    let pesos = calculatePesos(inputDollar);
+    if (pesos) {
+        writeResult(pesos, "dollar", "officialDollar", "ARS", 0);
+        calculateExchanges(pesos, "dollar");
 
     }
 
-});
+}
+
+function calculatePesos(amount) {
+
+    if (!isValidNumber(amount)) {
+        amount = null;
+    }
+
+    const exchangeRate = localStorage.getItem("officialDollar");
+
+    if (!exchangeRate) {
+        writeNoRate("dollar", "officialDollar");
+        euroRates.forEach(rate => {
+            writeNoRate("dollar", rate);
+        });
+        return;
+    }
+
+    if (!amount) {
+        writeBlank("dollar", "officialDollar", "ARS");
+        euroRates.forEach(rate => {
+            writeBlank("dollar", rate, "€");
+        });
+        return;
+    }
+
+    return parseInt(amount) * parseInt(exchangeRate);
+
+}
+
+//Calculate Exchange
+
+function calculateExchanges(amount, page) {
 
 
+    // Check if pesos is a valid number
+    if (!isValidNumber(amount)) {
+        amount = null;
+    }
 
+    euroRates.forEach(rate => {
+        const exchangeRate = localStorage.getItem(rate);
 
+        // Early return if no exchange rate is available
+        if (!exchangeRate) {
+            writeNoRate(page, rate);
+            return;
+        }
+
+        // Early return if pesos input is empty or invalid
+        if (!amount) {
+            writeBlank(page, rate, "€");
+            return;
+        }
+
+        // Calculate result and display it
+        let result = calculateExchange(parseInt(amount), parseInt(exchangeRate));
+        writeResult(result, page, rate, "€", 2);
+    });
+}
+
+function getElement(page, rate) {
+    return document.querySelector(`.output.${page}.${rate}`);
+
+}
+
+function writeResult(result, page, rate, currency, decimal) {
+    const element = getElement(page, rate);
+    const output = result.toFixed(decimal).replace(".", ",");
+    element.innerHTML = `${output} ${currency}`;
+}
+
+function writeBlank(page, rate, currency) {
+    const element = getElement(page, rate);
+    if (currency === "ARS") {
+        element.innerHTML = `---- ${currency}`;
+        return;
+    }
+    element.innerHTML = `-.-- ${currency}`;
+}
+
+function writeNoRate(page, rate) {
+    const element = getElement(page, rate);
+    element.innerHTML = "Kein Kurs";
+    element.classList.add("no-rate");
+}
+
+function calculateExchange(amount, rate) {
+    return amount / rate;
+}
+
+function isValidNumber(value) {
+    if (typeof value === "string") {
+        return !isNaN(value) && value.trim() !== '';
+    } else if (typeof value === "number") {
+        return isFinite(value);
+    }
+    return false;
+}
 
 
 
